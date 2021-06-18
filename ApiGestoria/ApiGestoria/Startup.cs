833315efc1f0bd85f7909340e4aa582188a0f01d
AxelInetum <axel.picon@inetum.world>
2021-06-18 08:56:57 +0200

    ultima version docker compose   react + api + sql

--- ApiGestoria/ApiGestoria/Startup.cs
+++ ApiGestoria/ApiGestoria/Startup.cs
@@ -119,6 +119,19 @@ namespace ApiGestoria
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                System.Console.WriteLine("escribiendo migraciones");
+
+
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+
+                System.Console.WriteLine("migraciones actualizadas correctamente");
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
